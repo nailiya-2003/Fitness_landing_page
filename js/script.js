9460cf28353670e128817768868dfa2731e6ee84
Naila <naile.gashimova@gmail.com>
2023-05-20 13:18:25 +0400

    edit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 new WOW().init();
 
 
+const time = 100;
+const step = 1;
+
+function outNum(num, elem) {
+  let e = document.querySelector("#out");
+  n = 0;
+  let t = Math.round(time / (num / step));
+  let interval = setInterval(() => {
+    n = n + step;
+    if (n == num) {
+      clearInterval(interval);
+    }
+    e.innerHTML = n;
+  }, t);
+}
+
+outNum(350, "#out");
 
 
 
